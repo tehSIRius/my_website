140d89556ecb797fbdbae8332bf7cbedb85ac9b6
Patrik Dvořáček <patrik@dvoracek.dev>
2020-04-23 18:49:44 +0200

    Changed to Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `Patrik Dvořáček`,
+    description: `Hello, this is Patrik.`,
+    author: `@tehSIRius`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`
+      },
+    }
+  ],
+}
