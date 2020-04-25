2de1dbe7e606db83a65b3bf3dadf84a543a4927c
Patrik Dvořáček <patrik@dvoracek.dev>
2020-04-25 16:36:59 +0200

    Added 404 redirect to Index

--- src/components/content/HelmetHeader.js
+++ src/components/content/HelmetHeader.js
@@ -8,7 +8,7 @@ class HelmetHeader extends React.Component {
                 <title>Patrik Dvořáček</title>
                 <meta name="description" content="The personal page of Patrik Dvořáček." />
                 <meta name="theme-color" content="#ff5722" />
-                <meta name="og:title" property="og:title" conent="Patrik Dvořáček" />
+                <meta name="og:title" property="og:title" content="Patrik Dvořáček" />
                 <meta name="og:description" property="og:description" content="The personal page of Patrik Dvořáček." />
                 <meta name="og:type" content="website" />
                 <meta name="og:url" content="https://dvoracek.dev" />
